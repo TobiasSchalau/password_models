13ac59af4faa25b4b5488b0081069678560fc23f
TobiasSchalau <tobias.schalau@fu-berlin.de>
2021-05-21 10:06:20 +0200

    Skript für 1c

--- shoulder_surfer.py
+++ shoulder_surfer.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
-import matplotlib.pyplot as plt
-from pandas.plotting import table
 import dataframe_image as dfi
 
 if __name__ == '__main__':
